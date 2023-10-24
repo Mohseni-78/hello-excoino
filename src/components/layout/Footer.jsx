0e5c42cff1765f626551cf07399204931ed1855f
alireza mohseni <al.mohseni@ernyka.com>
2023-10-24 11:12:58 +0330

    fds

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-[#110527] flex items-center justify-center py-5 md:hidden mt-[22rem] text-white">
+    <div className="bg-[#110527] flex items-center justify-center py-5 md:hidden mt-[24rem] md:mt-0 text-white">
       طلاعات تماس:
     </div>
   );
