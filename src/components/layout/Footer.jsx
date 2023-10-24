dd52629abb3241b031c892baa0c1284f60d8c1e4
alireza mohseni <al.mohseni@ernyka.com>
2023-10-24 11:27:10 +0330

    gfd

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,15 @@
+import { FaPhone } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 const Footer = () => {
   return (
-    <div className="bg-[#110527] flex items-center justify-center py-5 md:hidden mt-[24rem] md:mt-0 text-white">
-      طلاعات تماس:
+    <div className="bg-[#110527] flex items-center justify-center gap-4 py-5 md:hidden mt-[24rem] md:mt-0 text-white">
+      <p>اطلاعات تماس:</p>
+      <a href="tel:02128111400">
+        <FaPhone />
+      </a>
+      <a href="mailto:support@excoino.com">
+        <IoMdMail />
+      </a>
     </div>
   );
 };
