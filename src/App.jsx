f0dc3c265c7e7a76fff8565f637c6db30942bc6e
alireza mohseni <al.mohseni@ernyka.com>
2023-10-24 11:19:20 +0330

    fds

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <img src={headerBg} alt="" className="absolute w-full h-full hidden md:block z-[-99]" />
-      <img src={bgFooter} alt="" className="absolute bottom-[3rem]" />
+
       <img src={gradient} alt="" className="absolute left-0 z-[-1] md:z-1" />
       {/* container */}
       <div className="md:container px-4 md:px-0 ">
@@ -76,6 +76,7 @@ function App() {
       </div>
       {/* images */}
       <div className="relative md:hidden mt-[20rem] md:mt-0">
+        <img src={bgFooter} alt="" className="absolute bottom-[-4rem]" />
         <img src={medal} className="absolute left-0  bottom-[46px] w-[12rem]" />
         <img src={medal2} className="absolute left-[25vw]  bottom-[46px] z-[99] w-[12rem]" />
         <img src={medal3} className="absolute right-0 bottom-[46px] w-[12rem]" />
